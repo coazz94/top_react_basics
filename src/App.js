79eded6a5a87fa5879b71d38fc41fe952eed7e0b
coazz <aco94@hotmail.com>
2023-01-10 21:31:54 +0100

    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import userEvent from '@testing-library/user-event';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+  const formatName = (user) => {
+    return user.firstName + " " + user.lastName
+  }
+
+  const user = {
+    firstName: "Aleksandar",
+    lastName: "Popovic",
+  }
+
+  const greetUser = (user) => {
+    if (user) return <h1>Hello, {formatName(user)}</h1>;
+    return <h1>Hello, Stranger</h1>;
+  }
+
+  // const emptyElement = <img src={user.avatarUrl} />;
+
+  const elementChildren = (
+    <div>
+        <p>-------------Element with children -------------</p>
+        <h1>Hello!</h1>
+        <h2>Good to see you here</h2>
+        <p>{formatName(user)}</p>
     </div>
-  );
-}
+  )
+
+  const link = <a href="https://www.reactjs.org"> Link </a>
+
+  const App = () => {
+    const element = (<h1>Hello, TEST</h1>)
+    const greet = greetUser(user);
+    return (<div>{[element, link, greet, elementChildren]}</div>)
+  }
+
+
+
+
 
 export default App;
